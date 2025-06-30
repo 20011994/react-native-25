f21f10b7bc7806afafbc611f1b279d2275646d88
love20011994 <love.kalsangrah@gmail.com>
2025-06-30 10:02:36 +0530

    auth-comlited

---
+++ LoginScreen.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+
+
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+const navigation = useNavigation();
+    const handleLogin = () => {
+      console.log("pass")
+        if (username === 'kushkal' && password === 'kush123') {
+            console.log(username, password, "kk")
+            
+   navigation.navigate('Dashboard');
+
+  } else {
+    alert("wrong password")
+  }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>Justdial</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ff6600', // Justdial orange
+    marginBottom: 40,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#fff',
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 8,
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: '#ff6600',
+    padding: 15,
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+});
